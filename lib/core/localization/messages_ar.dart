2c69589ded018d843cb62ca1ac151ea1067d23d9
seesupport <127085082+seesupport@users.noreply.github.com>
2026-07-10 22:12:50 +0300

    Add messages_ar.dart

---
+++ lib/core/localization/messages_ar.dart
@@ -0,0 +1,8 @@
+// lib/core/localization/messages_ar.dart
+class MessagesAr {
+  static const Map<String, String> messages = {
+    'app_title': 'تطبيقي',
+    'login': 'تسجيل الدخول',
+    'otp': 'رمز التحقق',
+  };
+}
